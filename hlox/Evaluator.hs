77208922a42988f04b6a0ccedc67b273a49632be
Sup#2.0 <sup2point0@gmail.com>
2026-05-18 16:00:03 +0100

    dev - refactor evaluator functions to use `StateT` monad

--- hlox/Evaluator.hs
+++ hlox/Evaluator.hs
@@ -2,6 +2,8 @@ module Evaluator where
 
 import Data.Maybe qualified as Maybe
 
+import Control.Monad.State
+
 import Debug.Trace (trace)
 
 import Evaluator.Objects qualified as Obj
@@ -16,197 +18,190 @@ import Parser.Ast (Program, Node)
 import Parser.Ops qualified as Op
 
   
-type EvalResult = (EvalObject, EvalEnv)
+type Evaluator env = StateT env (Either EvalError) EvalObject
 
 
 -- | Evaluate a Lox program, given its AST.
 evalProgram :: Program -> Either EvalError EvalObject
-evalProgram ast = go ast Env.new
+evalProgram ast = evalStateT (go ast) Env.new
   where
-    go :: Program -> EvalEnv -> Either EvalError EvalObject
+    go :: Program -> Evaluator EvalEnv
     
-    go [] _ = Right (Obj.Nil)
-    
-    go [node] env = do
-      (out, _) <- eval node env
-      return out
-    
-    go (node:nodes) env = do
-      (_, env') <- eval node env
-      go nodes env'
-      
+    go []           = lift $ Right (Obj.Nil)
+    go [node]       = eval node
+    go (node:nodes) = eval node >> go nodes
+
 
 -- | Evaluate a node in the AST.
-eval :: Node -> EvalEnv -> Either EvalError EvalResult
+eval :: Node -> Evaluator EvalEnv
 
-eval (Ast.Block nodes) env = go nodes (Env.from env)
+eval (Ast.Block nodes) = do
+    env <- get
+    (Env.ScopedEnv env') <- lift $ execStateT (go nodes) (Env.from env)
+    put env'
+    return Obj.Nil
   where
-    go :: [Ast.Node] -> ScopedEnv -> Either EvalError EvalResult
+    go :: [Ast.Node] -> Evaluator ScopedEnv
+    go [] = return Obj.Nil
+    go (stmt:stmts) = do
+      (Env.ScopedEnv env) <- get
+      env' <- lift $ execStateT (eval stmt) env
+      put $ Env.ScopedEnv env'
+      go stmts 
 
-    go [] env
-      = Right (Obj.Nil, Env.close env)
+eval (Ast.DeclVar ident node) = do
+  val <- eval node
+  modify $ Env.define ident val
+  return val
 
-    go (stmt:stmts) (Env.ScopedEnv env) = do
-      (_, env') <- eval stmt env
-      go stmts (Env.ScopedEnv env')
-
-eval (Ast.DeclVar ident node) env = do
-  (val, env') <- eval node env
-  return (val, Env.define ident val env')
-
-eval (Ast.DeclFunc ident params body) env = do
+eval (Ast.DeclFunc ident params body) = do
   let func = Obj.Callable ident params body
-  return (Obj.Nil, Env.define ident func env)
+  modify $ Env.define ident func
+  return Obj.Nil
 
-eval (Ast.Return mnode) env = do
-  (val, _) <- case mnode of
-    Just node -> eval node env
-    Nothing   -> Right (Obj.Nil, env)
-  Left (Err.Returning val)
+eval (Ast.Return mnode) = do
+  val <- case mnode of
+    Just node -> eval node
+    Nothing   -> lift $ Right Obj.Nil
+  lift $ Left (Err.Returning val)
 
-eval (Ast.Print node) env = do
-  (node', env') <- eval node env
-  return (trace ("hlox> " ++ show node') Obj.Nil, env')
+eval (Ast.Print node) = do
+  node' <- eval node
+  let print = trace ("hlox> " ++ show node')
+  return $ print Obj.Nil
 
-eval (Ast.If cond body) env = do
-  (cond', env') <- eval cond env
+eval (Ast.If cond body) = do
+  cond' <- eval cond
   case cond' of
-    Obj.Boolean True  -> eval body env'
-    Obj.Boolean False -> return (Obj.Nil, env')
-    ex                -> Left (Err.TypeError "boolean" (showType ex))
+    Obj.Boolean True  -> eval body
+    Obj.Boolean False -> return Obj.Nil
+    ex                -> lift $ Left (Err.TypeError "boolean" (showType ex))
 
-eval (Ast.IfElse cond true false) env = do
-  (cond', env') <- eval cond env
+eval (Ast.IfElse cond true false) = do
+  cond' <- eval cond
   case cond' of
-    Obj.Boolean True  -> eval true env'
-    Obj.Boolean False -> eval false env'
-    ex                -> Left (Err.TypeError "boolean" (showType ex))
+    Obj.Boolean True  -> eval true
+    Obj.Boolean False -> eval false
+    ex                -> lift $ Left (Err.TypeError "boolean" (showType ex))
 
-eval node@(Ast.While cond body) env = do
-  (cond', env') <- eval cond env
+eval node@(Ast.While cond body) = do
+  cond' <- eval cond
   case cond' of
-    Obj.Boolean True  -> do
-      (_, env'') <- eval body env'
-      eval node env''
-    Obj.Boolean False -> return (Obj.Nil, env')
-    ex                -> Left (Err.TypeError "boolean" (showType ex))
+    Obj.Boolean True  -> eval body >> eval node
+    Obj.Boolean False -> return Obj.Nil
+    ex                -> lift $ Left (Err.TypeError "boolean" (showType ex))
 
-eval (Ast.AsgnVar ident node) env = do
-  (val, env') <- eval node env
-  env'' <- Env.set ident val env'
-  return (val, env'')
+eval (Ast.AsgnVar ident node) = do
+  val <- eval node
+  env <- get
+  env' <- lift $ Env.set ident val env
+  put env'
+  return val
 
-eval (Ast.Call callee args) env = do
-    (callee', env') <- eval callee env
-    (args', env'') <- foldr go (Right ([], env')) args
-    call callee' args' env''
+eval (Ast.Call callee args) = do
+    callee' <- eval callee
+    args' <- go args
+    env <- get
+    call callee' args'
   where
-    go :: Ast.Node
-       -> Either EvalError ([EvalObject], EvalEnv)
-       -> Either EvalError ([EvalObject], EvalEnv)
-    go _ (Left e) = Left e
-    go node (Right (vals, env)) = do
-      (val, env') <- eval node env
-      return (val:vals, env')
+    go :: [Ast.Node] -> StateT EvalEnv (Either EvalError) [EvalObject]
+    go (node:nodes) = do
+      val <- eval node
+      vals <- go nodes
+      return (val:vals)
 
-eval (Ast.Binary Op.OR left right) env = do
-  (left', env') <- eval left env
+eval (Ast.Binary Op.OR left right) = do
+  left' <- eval left
   case left' of
-    Obj.Boolean True  -> return (left', env')
+    Obj.Boolean True  -> return left'
     Obj.Boolean False -> do
-      (right', env'') <- eval right env'
+      right' <- eval right
       case right' of
-        Obj.Boolean b -> return (right', env')
-        ex            -> Left (Err.TypeError "boolean" (showType ex))
-    ex -> Left (Err.TypeError "boolean" (showType ex))
+        Obj.Boolean b -> return right'
+        ex            -> lift $ Left (Err.TypeError "boolean" (showType ex))
+    ex -> lift $ Left (Err.TypeError "boolean" (showType ex))
 
-eval (Ast.Binary Op.AND left right) env = do
-  (left', env') <- eval left env
+eval (Ast.Binary Op.AND left right) = do
+  left' <- eval left
   case left' of
     Obj.Boolean b1 -> do
-      (right', env'') <- eval right env'
+      right' <- eval right
       case right' of
-        Obj.Boolean b2 -> return (Obj.Boolean (b1 && b2), env')
-        ex             -> Left (Err.TypeError "boolean" (showType ex))
-    ex -> Left (Err.TypeError "boolean" (showType ex))
+        Obj.Boolean b2 -> return (Obj.Boolean (b1 && b2))
+        ex             -> lift $ Left (Err.TypeError "boolean" (showType ex))
+    ex -> lift $ Left (Err.TypeError "boolean" (showType ex))
 
-eval (Ast.Unary Op.NEGATE node) env = do
-  (node', env') <- eval node env
+eval (Ast.Unary Op.NEGATE node) = do
+  node' <- eval node
   case node' of
-    Obj.Number n -> return (Obj.Number (-n), env')
-    ex           -> Left (Err.TypeError "number" (showType ex))
+    Obj.Number n -> return (Obj.Number (-n))
+    ex           -> lift $ Left (Err.TypeError "number" (showType ex))
 
-eval (Ast.Var ident) env
-  = case Env.get ident env of
-      Just val -> return (val, env)
-      Nothing  -> Left (Err.UndefinedVariable ident)
+eval (Ast.Var ident) = do
+  env <- get
+  case Env.get ident env of
+    Just val -> return val
+    Nothing  -> lift $ Left (Err.UndefinedVariable ident)
 
--- forwarding required for nice eta reduction on the rest
-eval node env = eval' node env
+-- TODO reorder
+eval (Ast.Stmt node) = eval node
 
+eval (Ast.Binary Op.EQ   l r) = evalBinaryEqOrd (==) l r
+eval (Ast.Binary Op.NEQ  l r) = evalBinaryEqOrd (/=) l r
+eval (Ast.Binary Op.LT   l r) = evalBinaryEqOrd (<)  l r
+eval (Ast.Binary Op.LTEQ l r) = evalBinaryEqOrd (<=) l r
+eval (Ast.Binary Op.GT   l r) = evalBinaryEqOrd (>)  l r
+eval (Ast.Binary Op.GTEQ l r) = evalBinaryEqOrd (>=) l r
 
-eval' :: Node -> EvalEnv -> Either EvalError EvalResult
+eval (Ast.Binary Op.ADD  l r) = evalBinaryArithmetic (+) l r
+eval (Ast.Binary Op.SUB  l r) = evalBinaryArithmetic (-) l r
+eval (Ast.Binary Op.MULT l r) = evalBinaryArithmetic (*) l r
+eval (Ast.Binary Op.DIV  l r) = evalBinaryArithmetic (/) l r
 
-eval' (Ast.Stmt node) = eval node
-
-eval' (Ast.Binary Op.EQ   left right) = evalBinaryEqOrd (==) left right
-eval' (Ast.Binary Op.NEQ  left right) = evalBinaryEqOrd (/=) left right
-eval' (Ast.Binary Op.LT   left right) = evalBinaryEqOrd (<)  left right
-eval' (Ast.Binary Op.LTEQ left right) = evalBinaryEqOrd (<=) left right
-eval' (Ast.Binary Op.GT   left right) = evalBinaryEqOrd (>)  left right
-eval' (Ast.Binary Op.GTEQ left right) = evalBinaryEqOrd (>=) left right
-
-eval' (Ast.Binary Op.ADD  left right) = evalBinaryArithmetic (+) left right
-eval' (Ast.Binary Op.SUB  left right) = evalBinaryArithmetic (-) left right
-eval' (Ast.Binary Op.MULT left right) = evalBinaryArithmetic (*) left right
-eval' (Ast.Binary Op.DIV  left right) = evalBinaryArithmetic (/) left right
-
-eval' (Ast.Str str) = return . (Obj.String str,)
-eval' (Ast.Num n)   = return . (Obj.Number n,)
-eval' (Ast.Bool b)  = return . (Obj.Boolean b,)
-eval' (Ast.Nil)     = return . (Obj.Nil,)
-eval' node          = const  $ Left (Err.UnknownError (show node))
+eval (Ast.Str str) = return $ Obj.String str
+eval (Ast.Num n)   = return $ Obj.Number n
+eval (Ast.Bool b)  = return $ Obj.Boolean b
+eval (Ast.Nil)     = return $ Obj.Nil
+eval node          = lift   $ Left (Err.UnknownError (show node))
 
 
 evalBinaryEqOrd :: (forall t. (Eq t, Ord t) => t -> t -> Bool)
                 -> Ast.Node -> Ast.Node
-                -> EvalEnv
-                -> Either EvalError EvalResult
+                -> Evaluator EvalEnv
 
-evalBinaryEqOrd op left right env = do
-  (left' , env')  <- eval left env
-  (right', env'') <- eval right env'
+evalBinaryEqOrd op left right = do
+  left'  <- eval left
+  right' <- eval right
 
   case (left', right') of
-    (Obj.Nil      , Obj.Nil      ) -> return (Obj.Boolean True      , env'')
-    (Obj.Boolean l, Obj.Boolean r) -> return (Obj.Boolean (l `op` r), env'')
-    (Obj.Number  l, Obj.Number  r) -> return (Obj.Boolean (l `op` r), env'')
-    (Obj.String  l, Obj.String  r) -> return (Obj.Boolean (l `op` r), env'')
-    _ -> Left (Err.MonoTypeError (showType left') (showType right'))
+    (Obj.Nil      , Obj.Nil      ) -> return $ Obj.Boolean True
+    (Obj.Boolean l, Obj.Boolean r) -> return $ Obj.Boolean (l `op` r)
+    (Obj.Number  l, Obj.Number  r) -> return $ Obj.Boolean (l `op` r)
+    (Obj.String  l, Obj.String  r) -> return $ Obj.Boolean (l `op` r)
+    _ -> lift $ Left (Err.MonoTypeError (showType left') (showType right'))
 
 
 evalBinaryArithmetic :: (Float -> Float -> Float)
                      -> Ast.Node -> Ast.Node
-                     -> EvalEnv
-                     -> Either EvalError EvalResult
+                     -> Evaluator EvalEnv
 
-evalBinaryArithmetic op left right env = do
-  (left',  env')  <- eval left env
-  (right', env'') <- eval right env'
+evalBinaryArithmetic op left right = do
+  left'  <- eval left
+  right' <- eval right
 
   case (left', right') of
-    (Obj.Number l, Obj.Number r) -> return (Obj.Number (l `op` r), env'')
-    (Obj.Number _, r           ) -> Left (Err.TypeError "Number" (showType r))
-    (l           , Obj.Number _) -> Left (Err.TypeError "Number" (showType l))
-    (l           , r           ) -> Left (Err.MonoTypeError (showType l) (showType r))
+    (Obj.Number l, Obj.Number r) -> return $ Obj.Number (l `op` r)
+    (Obj.Number _, r           ) -> lift   $ Left (Err.TypeError "Number" (showType r))
+    (l           , Obj.Number _) -> lift   $ Left (Err.TypeError "Number" (showType l))
+    (l           , r           ) -> lift   $ Left (Err.MonoTypeError (showType l) (showType r))
 
 
 call :: EvalObject
      -> [EvalObject]
-     -> EvalEnv
-     -> Either EvalError EvalResult
-call (Obj.Callable _ params body) args env = do
+     -> Evaluator EvalEnv
+call (Obj.Callable _ params body) args = do
+  env <- get
   let env' = foldr (uncurry Env.define) (Env.global env) (zip params args)
-  case eval body env' of
-    Left (Err.Returning res) -> return (res, env)
-    result                   -> result
+  case evalStateT (eval body) env' of
+    Left (Err.Returning r) -> return r
+    result                 -> lift result
